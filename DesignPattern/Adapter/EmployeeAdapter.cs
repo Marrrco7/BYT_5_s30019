07f560efce59ae31704a4e8542f82aadde12e084
Marco Zetina <m.zetina@arkitech.eu>
2025-11-28 21:19:24 +0100

    Adapter pattern done

--- DesignPattern/Adapter/EmployeeAdapter.cs
+++ DesignPattern/Adapter/EmployeeAdapter.cs
@@ -3,11 +3,24 @@ namespace DesignPattern.Adapter
 {
     public class EmployeeAdapter : ITarget
     {
-        private readonly BillingSystem thirdPartyBillingSystem = new();
+        private readonly BillingSystem _thirdPartyBillingSystem = new();
 
-        //public void ProcessCompanySalary(...)
-        //{
-        //    ...
-        //}
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            var employeesList = new List<Employee>();
+            var rows = employeesArray.GetLength(0);
+
+            for (var i = 0; i < rows; i++)
+            {
+                var idStr = employeesArray[i, 0];
+                var name = employeesArray[i, 1];
+                var designation = employeesArray[i, 2];
+                var salaryStr = employeesArray[i, 3];
+
+                var employee = new Employee(int.Parse(idStr), name, designation, decimal.Parse(salaryStr));
+                employeesList.Add(employee);
+            }
+            _thirdPartyBillingSystem.ProcessSalary(employeesList);
+        }
     }
 }
